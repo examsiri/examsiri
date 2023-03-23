f36e0ccca15235b4e7a2546b511575e395da8308
examsnet <57344633+examsnet@users.noreply.github.com>
2023-03-23 15:54:48 +0000

    Fixed.

---
+++ js/commons.js
@@ -0,0 +1,12 @@
+function getQueryStringByKey(key){
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    if(params[key]){
+        return params[key];
+    }else{
+        return '';
+    }
+}
+
+var gcbseurl="https://script.google.com/macros/s/AKfycby0B8Rmda8qEwItVG7x-F1TRWaA0GsEQf6k3cn4q0tjcuzBe8W7kJ-qFMrSIRMd_vaJ/exec"
