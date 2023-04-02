68b924dabce450629b20f9fe8315320b6e1e3ad0
examsnet <57344633+examsnet@users.noreply.github.com>
2023-04-02 23:45:46 +0100

    Fixed

--- js/commons.js
+++ js/commons.js
@@ -1,3 +1,4 @@
+var globaltimeout = 0;
 function getQueryStringByKey(key){
     const params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
@@ -7,7 +8,4 @@ function getQueryStringByKey(key){
     }else{
         return '';
     }
-}
-
-//var gcbseurl="https://script.google.com/macros/s/AKfycby0B8Rmda8qEwItVG7x-F1TRWaA0GsEQf6k3cn4q0tjcuzBe8W7kJ-qFMrSIRMd_vaJ/exec"
-//var gcbse10url = "https://script.google.com/macros/s/AKfycbwqsn0LiJjK7TXwGGnfotar0XuzgFqJ9WTLL5hANKXESaHqQT3GQSDzusXlmziUVeE/exec"
+} 
